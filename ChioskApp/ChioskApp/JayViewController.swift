2efb50285deff2e07347542bedd9bf056170eea6
Quaker-Lee <tkacl2010@gmail.com>
2024-11-25 14:44:26 +0900

    🔥 [TEST] 충돌 테스트

---
+++ ChioskApp/ChioskApp/JayViewController.swift
@@ -0,0 +1,14 @@
+//
+//  JayViewController.swift
+//  ChioskApp
+//
+//  Created by 이재건 on 11/25/24.
+//
+
+import Foundation
+
+class JayViewController {
+    func test() {
+        print("테스트")
+    }
+}
